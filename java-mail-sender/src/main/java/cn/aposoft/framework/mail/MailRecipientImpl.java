527190ed914e309d71f93300ed0bce8d95c769eb
pipi668 <pleasantboy@qq.com>
2016-03-13 20:36:17 +0800

    添加邮件接收人信息的注释

--- java-mail-sender/src/main/java/cn/aposoft/framework/mail/MailRecipientImpl.java
+++ java-mail-sender/src/main/java/cn/aposoft/framework/mail/MailRecipientImpl.java
@@ -12,13 +12,22 @@ import javax.mail.Message.RecipientType;
  *
  */
 public class MailRecipientImpl extends MailContactImpl implements MailRecipient {
-
 	private RecipientType type = RecipientType.TO;
 
+	/**
+	 * 设置接收人类型
+	 * 
+	 * @param type
+	 *            {@link RecipientType}
+	 * 
+	 */
 	public void setType(RecipientType type) {
 		this.type = type;
 	}
 
+	/**
+	 * 读取接收人类型
+	 */
 	@Override
 	public RecipientType getType() {
 		return type;
