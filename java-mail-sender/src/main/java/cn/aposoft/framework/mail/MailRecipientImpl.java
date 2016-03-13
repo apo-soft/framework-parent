4c935d814eb534f5d8e9f4f9f0391d6b1a4e8dee
pipi668 <pleasantboy@qq.com>
2016-03-13 16:30:10 +0800

    增加邮件接收人设置接收人类型的公共方法

--- java-mail-sender/src/main/java/cn/aposoft/framework/mail/MailRecipientImpl.java
+++ java-mail-sender/src/main/java/cn/aposoft/framework/mail/MailRecipientImpl.java
@@ -15,6 +15,10 @@ public class MailRecipientImpl extends MailContactImpl implements MailRecipient 
 
 	private RecipientType type = RecipientType.TO;
 
+	public void setType(RecipientType type) {
+		this.type = type;
+	}
+
 	@Override
 	public RecipientType getType() {
 		return type;
