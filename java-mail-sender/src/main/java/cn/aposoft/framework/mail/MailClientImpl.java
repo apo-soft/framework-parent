c9a261978be93d73a79f687c6ea9c27b4d2302e1
pipi668 <pleasantboy@qq.com>
2016-03-13 16:10:18 +0800

    提交MailClientImpl添加共有构造函数

--- java-mail-sender/src/main/java/cn/aposoft/framework/mail/MailClientImpl.java
+++ java-mail-sender/src/main/java/cn/aposoft/framework/mail/MailClientImpl.java
@@ -2,7 +2,15 @@ package cn.aposoft.framework.mail;
 
 import java.util.Collection;
 
+import javax.mail.Session;
+
 public class MailClientImpl implements MailClient {
+
+	private Session session;
+
+	public MailClientImpl(Session session) {
+		this.session = session;
+	}
 
 	@Override
 	public void send(MailContact from, MailReceiver to, String subject, String content) {
@@ -26,7 +34,6 @@ public class MailClientImpl implements MailClient {
 	public void send(MailContact from, Collection<MailReceiver> tos, Collection<MailReceiver> ccs,
 			MailMessage message) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
